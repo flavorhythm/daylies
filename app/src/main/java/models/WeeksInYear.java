6b2a3b61d71c756fec575da0c86201ae36f807f1
flavorhythm <zeyuki@gmail.com>
2016-03-02 16:56:28 -0600

    Separated WeeksInYear from PickerAdapter

---
+++ app/src/main/java/models/WeeksInYear.java
@@ -0,0 +1,15 @@
+package models;
+
+/**
+ * Created by zyuki on 3/2/2016.
+ */
+public class WeeksInYear {
+    private int weekNum;
+    private long date;
+
+    public int getWeekNum() {return weekNum;}
+    public long getDate() {return date;}
+
+    public void setWeekNum(int weekNum) {this.weekNum = weekNum;}
+    public void setDate(long date) {this.date = date;}
+}
