892f58fc48741558fe0b453f7a2b358e7fc20996
flavorhythm <zeyuki@gmail.com>
2016-03-04 11:49:52 -0600

    Implemented slideuppane with listview

--- app/src/main/java/com/example/zyuki/daylies/MainActivity.java
+++ app/src/main/java/com/example/zyuki/daylies/MainActivity.java
@@ -2,35 +2,35 @@ package com.example.zyuki.daylies;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import com.melnykov.fab.FloatingActionButton;
+import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 
 import adapters.MainAdapter;
-import data.DataAccessObject;
 import models.Day;
-import models.DayName;
 import models.ToDo;
 import utils.DateCalcs;
 import utils.ListBuilder;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
     private List<Day> week;
-    private List<ToDo> toDoList;
+    private List<String> dailyToDoStr;
+    private ArrayAdapter<String> toDoAdapter;
 
     private TextView yearText, weekNumText;
-    private ListView dayList;
+    private ListView dayList, toDoList;
+    private SlidingUpPanelLayout slider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,8 +46,15 @@ public class MainActivity extends AppCompatActivity {
         yearText.setText(String.valueOf(week.get(0).getYear()));
         weekNumText.setText(DateCalcs.addZeroToNum(week.get(0).getWeekNum()));
 
-        MainAdapter adapter = new MainAdapter(MainActivity.this, R.layout.main_row, week);
-        dayList.setAdapter(adapter);
+        dailyToDoStr = new ArrayList<>();
+
+        MainAdapter mainAdapter = new MainAdapter(MainActivity.this, R.layout.main_row, week);
+        dayList.setAdapter(mainAdapter);
+
+        toDoAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, dailyToDoStr);
+        toDoList.setAdapter(toDoAdapter);
+
+        dayList.setOnItemClickListener(this);
     }
 
     @Override
@@ -74,10 +81,25 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        dailyToDoStr.clear();
+        List<ToDo> dailyToDo = week.get(position).getToDoList();
+
+        if(dailyToDo.isEmpty()) {
+            dailyToDoStr.add("Nothing to do on " + week.get(position).getDay().toString());
+            toDoAdapter.notifyDataSetChanged();
+        }
+
+        slider.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
+    }
+
     private void findViewsById() {
         yearText = (TextView)findViewById(R.id.main_text_year);
         weekNumText = (TextView)findViewById(R.id.main_text_weekNum);
         dayList = (ListView)findViewById(R.id.main_list_daily);
+        slider = (SlidingUpPanelLayout)findViewById(R.id.main_sliding_layout);
+        toDoList = (ListView)findViewById(R.id.main_list_todo);
     }
 
     private void buildWeek() {
